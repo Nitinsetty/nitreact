bded0f241d9f745af0114fc7daedd06628c156dd
Your Correct Username <your-email@example.com>
2024-11-14 15:20:32 +0530

    new

--- src/Pages/Newmobile.jsx
+++ src/Pages/Newmobile.jsx
@@ -6,7 +6,7 @@ function Mobile() {
     return (
         <div className="mobile-container">
             {mobdata.map((item) => (
-                <Link to={`/mobile/${item.id}`}>
+                <Link to={`/mobile/${item.id}`} style={{textDecoration:"none", color:"black"}}>
                 <div className="product-item"> 
                     <div>
                         <img src={item.img} alt={item.Brand} className='attach' />
